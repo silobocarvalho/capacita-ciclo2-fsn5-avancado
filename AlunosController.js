3b593ef9e558ded4303eec486d596ecdd6001e24
“Sidartha <silobocarvalho@gmail.com>
2025-08-19 20:12:21 -0300

    CRUD Aluno

--- AlunosController.js
+++ AlunosController.js
@@ -1,39 +1,73 @@
 const { PrismaClient } = require('./generated/prisma');
 const prisma = new PrismaClient();
 
-exports.criarAluno = (req, res) => {
+exports.criarAluno = async (req, res) => {
     const {nome, matricula, turma} = req.body
+    
+    const novoAlunoCriado = await prisma.Aluno.create({
+        data: {
+            nome: nome,
+            matricula: matricula,
+            turma: turma
+        }
+    });
+    
     let msg = `O aluno ${nome} criado com sucesso!`
     console.log(msg)
-    res.status(201).json(msg);
+    res.status(201).json(novoAlunoCriado);
 };
 
 exports.listarAlunos = async (req, res) => {
-    //let msg = "Todos alunos listados com sucesso!"
-    //console.log(msg)
 
     const alunosDoBancoDeDados = await prisma.Aluno.findMany();
     console.log(alunosDoBancoDeDados);
     res.status(200).json(alunosDoBancoDeDados);
 };
 
-exports.buscarAlunoPorId = (req, res) => {
+exports.buscarAlunoPorId = async (req, res) => {
     const {id} = req.params;
+    
+    const alunoDoBanco = await prisma.Aluno.findFirst({
+        where:{
+            id: parseInt(id)
+        }
+    })
+    
     let msg = `Aluno com id ${id} buscado com sucesso!`
     console.log(msg)
-    res.status(200).send(msg);
+    res.status(200).send(alunoDoBanco);
 };
 
-exports.atualizarAluno = (req, res) => {
+exports.atualizarAluno = async (req, res) => {
     const {id} = req.params;
+    const {nome, matricula, turma} = req.body
+
+    const alunoAtualizado = await prisma.Aluno.update({
+        where:{
+            id: parseInt(id)
+        },
+        data:{
+            nome: nome,
+            matricula: matricula,
+            turma: turma
+        }
+    })
+
     let msg = `Aluno com id ${id} atualizado com sucesso!`
     console.log(msg)
-    res.status(200).send(msg);
+    res.status(200).send(alunoAtualizado);
 };
 
-exports.deletarAluno = (req, res) => {
+exports.deletarAluno = async (req, res) => {
     const {id} = req.params;
+
+    const alunoDeletado = await prisma.Aluno.delete({
+        where:{
+            id: parseInt(id)
+        }
+    })
+
     let msg = `Aluno com id ${id} deletado com sucesso!`
     console.log(msg)
-    res.status(200).send(msg);
+    res.status(200).send(alunoDeletado);
 };
